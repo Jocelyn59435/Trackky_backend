40e7ce0b784a78e8d263ad884f42e43a11054ee7
unknown <jocelynhuang193@outlook.com>
2021-09-08 12:51:44 +1000

    fix knex file 08

--- knexfile.ts
+++ knexfile.ts
@@ -1,21 +1,42 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 
 import dotenv from 'dotenv';
+import path from 'path';
 
-dotenv.config();
+dotenv.config({ path: path.join(__dirname, '.env') });
 
-const { POSTGRES_HOST, POSTGRES_DB, POSTGRES_USER, POSTGRES_PASSWORD } =
-  process.env;
+// const { POSTGRES_HOST, POSTGRES_DB, POSTGRES_USER, POSTGRES_PASSWORD } =
+//   process.env;
 
-module.exports = {
+// module.exports = {
+//   client: 'pg',
+//   connection: {
+//     host: POSTGRES_HOST,
+//     user: POSTGRES_USER,
+//     password: POSTGRES_PASSWORD,
+//     database: POSTGRES_DB,
+//   },
+//   ssl: {
+//     rejectUnauthorized: false,
+//   },
+// };
+
+function getConnection() {
+  if (process.env.DATABASE_USE_SSL) {
+    return process.env.DATABASE_URL + '?ssl=true';
+  }
+  return process.env.DATABASE_URL;
+}
+
+const connection = getConnection();
+const config = {
   client: 'pg',
-  connection: {
-    host: POSTGRES_HOST,
-    user: POSTGRES_USER,
-    password: POSTGRES_PASSWORD,
-    database: POSTGRES_DB,
-  },
+  connection: connection,
+  debug:
+    process.env.DEBUG_KNEX && process.env.DEBUG_KNEX.toLowerCase() !== 'false',
   ssl: {
     rejectUnauthorized: false,
   },
 };
+
+module.exports = config;
