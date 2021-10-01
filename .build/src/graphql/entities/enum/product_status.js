d681b6b74f8f0650fb17671c02f01ffb0942917d
Jocelyn59435 <63270296+Jocelyn59435@users.noreply.github.com>
2021-10-01 15:09:28 +1000

    Delete .build directory

--- .build/src/graphql/entities/enum/product_status.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.productStatus = void 0;
-var productStatus;
-(function (productStatus) {
-    productStatus["active"] = "active";
-    productStatus["finished"] = "finished";
-})(productStatus = exports.productStatus || (exports.productStatus = {}));
-//# sourceMappingURL=product_status.js.map
